9210e61b9e97425a01f57b0874f92b3ec81dba20
JorgeTerence <jorge.terence@outlook.com>
2022-10-28 20:42:16 -0300

    Added some links 🔗 and font awesome icons

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 const greeting = document.querySelector("#greeting");
+const themeToggle = document.querySelector(".theme-toggle")
 
 const delay = (ms) => new Promise((r) => setTimeout(r, ms));
 
 document.addEventListener("DOMContentLoaded", async () => {
   const steps = [
-    "I'm a high scool student...",
-    "I love cats ᓚᘏᗢ",
+    "I'm a high school student...",
+    "I love cats and programming ᓚᘏᗢ",
     "Leave a star ⭐",
     "¯\\_(ツ)_/¯",
   ];
 
+  await delay(800);
+
   for (const s of steps) {
+    greeting.classList.remove("blink");
     greeting.innerHTML = "";
 
     for (const c of s) {
       greeting.innerHTML += c;
-      await delay(200);
+      await delay(170);
     }
 
+    greeting.classList.add("blink");
     await delay(2000);
   }
 });
